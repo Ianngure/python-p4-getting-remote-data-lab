fbbf38435419e8d9468222a5613f83fdfbd8c78b
ianngure <kayngure57@gmail.com>
2024-07-08 22:50:20 +0300

    initial commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,14 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        return response.content  # Return the content as bytes
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
